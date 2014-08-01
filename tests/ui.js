6bdd1946da13cbb59a4f07b33a99a7ceec4b62d0
Gijsjan <agijsbro@gmail.com>
2014-08-01 14:15:33 +0200

    Fix search button

--- tests/ui.js
+++ tests/ui.js
@@ -1,13 +1,13 @@
-module.exports = {
-  'jQuery UI sortable': function(browser) {
-    browser
-      .url('http://jqueryui.com/resources/demos/sortable/default.html')
-      .waitForElementVisible('#sortable > li:first-child', 1000)
-      .moveToElement('#sortable > li:first-child', 10, 10)
-      .mouseButtonDown()
-      .moveTo(0, 60)
-      .mouseButtonUp()
-      .assert.containsText('#sortable > li:nth-child(2)', 'Item 1')
-      .end()
-  }
-}
+//module.exports = {
+//  'jQuery UI sortable': function(browser) {
+//    browser
+//      .url('http://jqueryui.com/resources/demos/sortable/default.html')
+//      .waitForElementVisible('#sortable > li:first-child', 1000)
+//      .moveToElement('#sortable > li:first-child', 10, 10)
+//      .mouseButtonDown()
+//      .moveTo(0, 60)
+//      .mouseButtonUp()
+//      .assert.containsText('#sortable > li:nth-child(2)', 'Item 1')
+//      .end()
+//  }
+//}
